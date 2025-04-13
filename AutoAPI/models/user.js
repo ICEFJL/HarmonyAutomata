e5fc852b0d72aac1e714959ac7eef87d00d2f390
岳林佳 <coding_support@tencent.com>
2025-04-13 20:48:07 +0800

    上传文件

---
+++ AutoAPI/models/user.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const bcrypt = require('bcryptjs');
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    uname: DataTypes.STRING,
+    upassword: DataTypes.STRING,
+    role: DataTypes.STRING
+  });
+  User.associate = (models) => {
+    User.hasMany(models.Answer, {foreignKey: 'student_id'});
+    User.hasMany(models.TeacherAndStudent, {foreignKey: 'teacher_id'})
+  };
+  User.init({
+    uname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    upassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value){
+        if(value.length >= 6 && value.length <= 45){
+          this.setDataValue('upassword',bcrypt.hashSync(value,10));
+        }else{
+          throw new Error('密码长度必须在6-45之间');
+        }
+      }
+    },
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
