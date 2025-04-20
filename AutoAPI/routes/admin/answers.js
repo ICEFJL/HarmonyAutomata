68887a702498f58d82a4c3af587f8064330e6b2e
岳林佳 <coding_support@tencent.com>
2025-04-20 18:05:01 +0800

    添加redis

--- AutoAPI/routes/admin/answers.js
+++ AutoAPI/routes/admin/answers.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { Answer } = require('../../models');
+const { Answer, Excercise} = require('../../models');
 const { success, failure } = require('../../utils/responses');
 const { BadRequestError, NotFoundError } = require('../../utils/errors');
 const bcrypt = require('bcryptjs');
+const { redisClient,setKey, getKey,delKey, getKeysByPattern} = require('../../utils/redis');
 
 /**
  * 查询答案列表
@@ -18,18 +19,37 @@ router.get('/', async function (req, res, next) {
         const pageSize = Math.abs(Number(query.pageSize)) || 2;
         //计算offset
         const offset = (currentPage - 1) * pageSize;
+
+        //定义带有[当前页码]和[每页条数]的cacheKey作为缓存的键
+        const cacheKey = `answers:${currentPage}:${pageSize}`;
+        //读取缓存中的数据
+        let data = await getKey(cacheKey);
+        if(data) {
+            return success(res, '查询答案列表成功。', data)
+        }
+
         const condition = {
             limit: pageSize,
             offset: offset
         };
-        if (query.title) {
-            condition.where = {
-                title: {
-                    [Op.like]: `%${query.title}%`
-                }
+        // if (query.title) {
+        //     condition.where = {
+        //         title: {
+        //             [Op.like]: `%${query.title}%`
+        //         }
+        //     }
+        // }
+        const {count, rows} = await Answer.findAndCountAll(condition);
+        data = {
+            excercises:rows,
+            pagination: {
+                total: count,
+                currentPage,
+                pageSize
             }
         }
-        const {count, rows} = await Answer.findAndCountAll(condition);
+        await setKey(cacheKey, data);
+
         success(res, '查询答案列表成功。', {
             excercises: rows.map(answer => ({
                 id: answer.id,
@@ -51,13 +71,23 @@ router.get('/', async function (req, res, next) {
     }
 });
 /**
- * 查询单个习题详情
- * get /admin/excercises/:id
+ * 查询单个答案详情
+ * get /admin/answers/:id
  */
 router.get('/:id', async function (req, res, next) {
     try {
-        const answer = await getAnswer(req);
-        success(res, '查询答案详情成功。', {answer});
+        const {id} = req.params;
+        let answer = await getKey(`answer:${id}`);
+        if(!answer){
+            answer = await Excercise.findByPk(id);
+            if (!answer) {
+                throw new NotFoundError(`ID: ${id}的答案未找到`);
+            }
+            await setKey(`answer:${id}`, answer);
+        }
+        success(res, '查询用户详情成功。', answer);
+        // const answer = await getAnswer(req);
+        // success(res, '查询答案详情成功。', {answer});
     } catch (error) {
         failure(res, error);
     }
@@ -78,6 +108,18 @@ async function getAnswer(req) {
     }
     return answer;
 }
-
+/**
+ * 清除缓存
+ */
+async function clearCache(id = null) {
+    const keys = await getKeysByPattern('answers:*');
+    if(keys.length !== 0) {
+        await delKey(keys);
+    }
+    if(id) {
+        const keys = await getKeysByPattern(`answer:${id}`);
+        await delKey(keys);
+    }
+}
 
 module.exports = router;
