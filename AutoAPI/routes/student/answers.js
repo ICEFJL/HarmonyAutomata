8746334a0c2b77ff61eb05ab2cff96664d33f568
岳林佳 <coding_support@tencent.com>
2025-04-20 18:06:18 +0800

    redis

--- AutoAPI/routes/student/answers.js
+++ AutoAPI/routes/student/answers.js
@@ -4,7 +4,7 @@ const { Answer } = require('../../models');
 const { success, failure } = require('../../utils/responses');
 const { BadRequestError, NotFoundError } = require('../../utils/errors');
 const bcrypt = require('bcryptjs');
-
+const { redisClient,setKey, getKey,delKey, getKeysByPattern} = require('../../utils/redis');
 /**
  * 向answers表提交答案
  * POST /student/answers
@@ -14,7 +14,8 @@ router.post('/', async function (req, res, next) {
         try {
             const body = filterBody(req);
             const answer = await Answer.create(body);
-            success(res, '答案提交成功。', {answer}, 201);
+            await clearCache(req.userId, body.excercise_id);
+            success(res, '答案提交成功。', {answer});
         } catch (error) {
             failure(res, error);
         }
@@ -22,21 +23,34 @@ router.post('/', async function (req, res, next) {
         failure(res, error);
     }
 })
+
 /**
  * 获取answers表的score
  * GET /student/answers/:excerciseId
  */
 router.get('/:excerciseId', async function (req, res, next) {
     try {
+        //定义带有[当前页码]和[每页条数]的cacheKey作为缓存的键
+        const cacheKey = `answers:${req.userId}:${req.params.excerciseId}`;
+        //读取缓存中的数据
+        let data = await getKey(cacheKey);
+        if(data) {
+            return success(res, `查询 答案 成功。`, data)
+        }
+
         const condition = {
             excercise_id: req.params.excerciseId,
             student_id: req.userId
         }
-        const answer = await Answer.findOne({
+        const answer = await Answer.findAll({
             where: condition
         });
+        //缓存数据
+        data = answer;
+        await setKey(cacheKey, data);
+
         if (answer) {
-            success(res, '获取答案成功。', {answer});
+            success(res, '获取答案成功。', data);
         } else {
             throw new NotFoundError('答案不存在。');
         }
@@ -55,5 +69,13 @@ function filterBody(req) {
         student_id: req.userId
     }
 }
-
+/**
+ * 清除缓存
+ */
+async function clearCache() {
+    const keys = await getKeysByPattern(`answers:*`);
+    if(keys.length !== 0){
+        await delKey(keys);
+    }
+}
 module.exports = router;
