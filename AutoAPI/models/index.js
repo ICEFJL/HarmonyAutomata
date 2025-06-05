ccec41961efba10be916bc84b713f95153fc8fd3
岳林佳 <coding_support@tencent.com>
2025-06-05 19:05:02 +0800

    更新文件 index.js

--- AutoAPI/models/index.js
+++ AutoAPI/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const env = process.env.NODE_ENV || 'development';
 
-// 关键修改1：使用静态路径加载配置文件
+// 使用静态路径加载配置文件
 const config = require('../config/config.json')[env];
 
 const db = {};
@@ -16,12 +16,12 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-// 关键修改2：手动导入所有模型（静态方式）
+//手动导入所有模型（静态方式）
 db.User = require('./User')(sequelize, Sequelize.DataTypes);
 db.Answer = require('./answer')(sequelize, Sequelize.DataTypes);
 db.TeacherAndStudent = require('./teacherAndStudent')(sequelize, Sequelize.DataTypes);
 db.Excercise = require('./excercise')(sequelize, Sequelize.DataTypes);
-// 继续添加其他模型...
+
 
 // 模型关联
 Object.keys(db).forEach(modelName => {
